3c8416afc6447568ed45340c0ee73d8d3b447332
GUchechi <godswilluchendu7@gmail.com>
2026-01-06 00:52:58 +0100

    strings

---
+++ strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    string greeting = "Hello";
+    string complete_greeting = greeting + " World!";
+    cout << complete_greeting << endl;
+
+
+    
+
+    return 0;
+}
