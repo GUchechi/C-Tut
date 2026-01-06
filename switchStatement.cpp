aa3db427ca011fe5d0e7b81bd235cd0c32febfe3
GUchechi <godswilluchendu7@gmail.com>
2026-01-06 18:42:30 +0100

    switch statement

---
+++ switchStatement.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    switch (age) {
+        case 0 ... 12:
+            cout << "You are a child." << endl;
+            break;
+        case 13 ... 17:
+            cout << "You are a teenager." << endl;
+            break;
+        case 18 ... 64:
+            cout << "You are an adult." << endl;
+            break;
+        case 65 ... 120:
+            cout << "You are a senior citizen." << endl;
+            break;
+        default:
+            cout << "Invalid age entered." << endl;
+            break;
+    }
+
+    
+
+    return 0;
+}
