7e91609aa47b5f41da5cc28d5970336152cb0f6c
GUchechi <godswilluchendu7@gmail.com>
2026-01-06 19:58:36 +0100

    Added simple password secret with do While loop

---
+++ doWhileLoop.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main()
+{
+    string password = "secret";
+    string guess;
+
+    do
+    {
+        cout << "Enter password: ";
+        cin >> guess;
+
+        if (guess != password)
+        {
+            cout << "Incorrect Password! Try again." << endl;
+        }
+
+    } while (guess != password);
+
+    cout << "Correct Password, Access Granted!" << endl;
+
+    return 0;
+}
--- forLoopFactorialCal.cpp
+++ forLoopFactorialCal.cpp
@@ -4,13 +4,12 @@ using namespace std;
 int main()
 {
 
-    int factorial;
     int fact;
 
     cout << "Enter a number to find its factorial: ";
     cin >> fact;
 
-    factorial = fact;
+   int factorial = fact;
     for (int i = factorial - 1; i >= 1; --i)
     {
         factorial *= i;
