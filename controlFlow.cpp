1932028e5c2f17305b47723fde7d222acf52e8c5
GUchechi <godswilluchendu7@gmail.com>
2026-01-06 18:28:55 +0100

    control flow

---
+++ controlFlow.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main()
+{
+
+    string name_answer = "Caleb";
+    int age_answer = 63;
+
+    string name_guess;
+    cout << "Who is the best programmer?" << endl;
+    getline(cin, name_guess);
+
+    int age_guess;
+    cout << "Guess my age:" << endl;
+    cin >> age_guess;
+
+    if (name_guess == name_answer && age_guess == age_answer)
+    {
+        cout << "You got it Correct!" << endl;
+    }
+    else
+    {
+        cout << "No, you're Incorrect!" << endl;
+    }
+
+    return 0;
+}
