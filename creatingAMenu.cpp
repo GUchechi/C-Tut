f670e9c6282873cf81e998ac110d937252c953fd
GUchechi <godswilluchendu7@gmail.com>
2026-01-08 23:57:41 +0100

    Create a menu game

---
+++ creatingAMenu.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+void playGame()
+{
+    int random = rand() % 251; // Random number between 0 and 250
+    cout << "Random number generated: " << random << endl;
+
+    while (true)
+    {
+        int guess;
+        cout << "Enter your guess (0-250): ";
+        cin >> guess;
+
+        if (guess < 0 || guess > 250)
+        {
+            cout << "Please enter a valid number between 0 and 250.\n";
+            continue;
+        }
+
+        if (guess < random)
+        {
+            cout << "Too low! Try again.\n";
+        }
+        else if (guess > random)
+        {
+            cout << "Too high! Try again.\n";
+        }
+        else
+        {
+            cout << "Congratulations! You've guessed the correct number: " << random << "\n";
+            break;
+        }
+    }
+}
+
+int main()
+{
+
+    srand(time(nullptr)); // Seed the random number generator
+
+    int choice;
+
+    do
+    {
+        cout << "0. Quit\n1. Play Game\n";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 0:
+            cout << "Quitting the game. Goodbye!\n";
+            return 0;
+        case 1:
+            playGame();
+            break;
+        }
+    } while (choice != 0);
+
+    return 0;
+}
