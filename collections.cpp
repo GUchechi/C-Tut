7572b7fcc89bcb5288cab64190a88268f2ac4517
GUchechi <godswilluchendu7@gmail.com>
2026-01-10 00:50:54 +0100

    refactor

--- collections.cpp
+++ collections.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void print_array(int arr[], int size) {
+    for (int i = 0; i < size; i++ ){
+        cout << arr[i] << "\t";
+    }
+}
+
 int main() {
     
-    int guesses[20];
-    std::cin >> guesses[0];
-    std::cout << "You entered: " << guesses[0] << std::endl;
+    int guesses[] = {10, 50, 100, 150, 200, 250};
+    int size = sizeof(guesses) / sizeof(guesses[0]);
 
+    // int size = sizeof(guesses) / sizeof(guesses[0]);
+    // for (int i = 0; i < size; i++ ){
+    //     cout << guesses[i] << endl;
+    // }
+    print_array(guesses, size);
     
 
     return 0;
