44de3802885c0953cd926e52b1cbb930ed6293a8
GUchechi <godswilluchendu7@gmail.com>
2026-01-11 01:42:06 +0100

    added refactoring

---
+++ multidimensionalArraysAndNestedVectors.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int main()
+{
+
+    vector<vector<int>> grades = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << grades[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
